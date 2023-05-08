49f94dd3400d647b9a74e572ca57faf5f1fe4886
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-05-08 19:11:39 +0900

    Feat: title 반응형 추가

--- client/src/styles/Title.jsx
+++ client/src/styles/Title.jsx
@@ -4,6 +4,9 @@ const Title = styled.h1`
 	font-size: var(--title);
 	color: var(--main-color);
 	font-weight: 800;
+	@media (max-width: 768px) {
+		font-size: var(--sub-title);
+	}
 `;
 
 export default Title;
