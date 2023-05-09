3dddaf373963dbf05c4b4c2c5df58f1f8d255143
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-05-09 21:10:12 +0900

    Feat: route연결

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,44 @@
+import { Route, Routes } from 'react-router-dom';
 import GlobalStyles from './styles/index';
+import MapPage from './pages/mapPage';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import Mypage from './pages/mypage';
+import EditMyPage from './pages/editmypage';
+import Community from './pages/Community';
+import CommunityDetail from './pages/CommunityDetail';
+import CommunityEdit from './pages/CommunityEdit';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {
+	URL_MAP,
+	URL_LOGIN,
+	URL_SIGNUP,
+	URL_MYPAGE,
+	URL_EDITMYPAGE,
+	URL_POST,
+	URL_POSTDETAIL,
+	URL_WRITEPOST,
+} from './routesURL';
 
 function App() {
 	return (
 		<>
 			<GlobalStyles />
-			<div>초기 세팅</div>
+			<Header />
+			<Routes>
+				<Route path={URL_MAP} element={<MapPage />} />
+				<Route path={URL_LOGIN} element={<Login />} />
+				<Route path={URL_SIGNUP} element={<Signup />} />
+				<Route path={URL_MYPAGE} element={<Mypage />} />
+				<Route path={URL_EDITMYPAGE} element={<EditMyPage />} />
+				<Route path={URL_POST} element={<Community />} />
+				<Route path={URL_POSTDETAIL} element={<CommunityDetail />} />
+				<Route path={URL_WRITEPOST} element={<CommunityEdit />} />
+			</Routes>
+			<Footer />
 		</>
-	)
+	);
 }
 
 export default App;
---
+++ client/src/routesURL.js
@@ -0,0 +1,8 @@
+export const URL_MAP = '/';
+export const URL_LOGIN = '/login';
+export const URL_SIGNUP = '/signup';
+export const URL_MYPAGE = '/mypage';
+export const URL_EDITMYPAGE = '/editmypage';
+export const URL_POST = '/post/read';
+export const URL_POSTDETAIL = '/post/read/:postId';
+export const URL_WRITEPOST = '/post/write';
