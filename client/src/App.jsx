90eb39b354adc3586dc455f4ac32f66be93fd200
Seulgi-Yoo <119473025+Seulgi-Yoo@users.noreply.github.com>
2023-05-09 18:23:42 +0900

    Revert: 잘못 머지한 브랜치 revert
    This reverts commit 4fde47cef4bf9c50231983a988f9dbc2d8fc264c.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,5 @@
-import GlobalStyles from './styles/index';
-
 function App() {
-	return (
-		<>
-			<GlobalStyles />
-			<div>초기 세팅</div>
-		</>
-	)
+	return <div>초기 세팅</div>;
 }
 
 export default App;
--- client/src/mediaQuery.js
+++
@@ -1,3 +0,0 @@
-const MOBILE_MAX_WIDTH = 767;
-
-export default MOBILE_MAX_WIDTH;
