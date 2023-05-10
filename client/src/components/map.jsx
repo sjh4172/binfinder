8836f42b13b32f60ccb32857460fdad8d058b452
RomaneeChoiti <tmd1568@gmail.com>
2023-05-10 17:20:20 +0900

    feat/지도 내 위치표시

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -1,7 +1,9 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import KAKAO_MAP_API_KEY from '../api/kakaoMap';
+import Modal from './Modal';
 
+// 맵사이즈
 const MapStyle = styled.div`
 	height: 100vh;
 	align-items: center;
@@ -19,54 +21,72 @@ const MapStyle = styled.div`
 `;
 
 function Map() {
-	// 스크립트 파일 읽어오기
-	const newScript = (src) => {
-		return new Promise((resolve, reject) => {
-			const script = document.createElement('script');
-			script.src = src;
-			script.addEventListener('load', () => {
-				resolve();
-			});
-			script.addEventListener('error', (e) => {
-				reject(e);
-			});
-			document.head.appendChild(script);
-		});
-	};
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-	useEffect(() => {
+	const loadKakaoMap = useCallback(() => {
 		// 카카오맵 스크립트 읽어오기
-		const myScript = newScript(
-			`https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${KAKAO_MAP_API_KEY}`,
-		);
-
-		// 스크립트 읽기 완료 후 카카오맵 설정
-		myScript.then(() => {
+		const script = document.createElement('script');
+		script.src = `https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${KAKAO_MAP_API_KEY}`;
+		script.onload = () => {
 			const { kakao } = window;
 			kakao.maps.load(() => {
 				const mapContainer = document.getElementById('map');
-				const options = {
-					center: new kakao.maps.LatLng(37.56000302825312, 126.97540593203321), // 좌표설정
-					level: 3,
-				};
-				const map = new kakao.maps.Map(mapContainer, options); // 맵생성
-				// 마커설정
-				const markerPosition = new kakao.maps.LatLng(
-					37.56000302825312,
-					126.97540593203321,
+				// 위치 초기 값을 자신에 위치로 설정함
+				navigator.geolocation.getCurrentPosition(
+					(position) => {
+						const lat = position.coords.latitude;
+						const lng = position.coords.longitude;
+						const options = {
+							center: new kakao.maps.LatLng(lat, lng),
+							level: 3,
+						};
+						const map = new kakao.maps.Map(mapContainer, options);
+						const markerPosition = new kakao.maps.LatLng(lat, lng);
+						const blueMarkerImage = new kakao.maps.MarkerImage(
+							`${process.env.PUBLIC_URL}/assets/myLocationIcon.png`,
+							new kakao.maps.Size(30),
+							{ offset: new kakao.maps.Point(12, 35) },
+						);
+						const marker = new kakao.maps.Marker({
+							position: markerPosition,
+							// 마커 디자인!
+							image: blueMarkerImage,
+						});
+						marker.setMap(map);
+					},
+					(error) => {
+						// 위치 정보를 가져오지 못할 경우 처리
+						console.error(error);
+						setIsModalOpen(true);
+					},
 				);
-				const marker = new kakao.maps.Marker({
-					position: markerPosition,
-				});
-				marker.setMap(map);
 			});
-		});
+		};
+		document.head.appendChild(script);
 	}, []);
 
+	useEffect(() => {
+		loadKakaoMap();
+	}, [loadKakaoMap]);
+
+	const handleModalConfirm = () => {
+		setIsModalOpen(false);
+	};
 	return (
-		<MapStyle>
-			<div id="map" className="map" />
-		</MapStyle>
+		<>
+			<MapStyle>
+				<div id="map" className="map" />
+			</MapStyle>
+			{isModalOpen && (
+				<Modal
+					message="현재 위치를 가져올 수 없습니다. 
+					<br>GPS기능이 활성화되어 있는지 확인해 주세요.
+					<br>GPS기능을 활성화 시킨 후 앱을 다시 다시해주세요."
+					handleConfirm={handleModalConfirm}
+					handleCancel={handleModalConfirm}
+				/>
+			)}
+		</>
 	);
 }
 
