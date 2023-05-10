abb6c9184e4c1fe8caf2ffcdd11319437ac744d8
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-05-10 16:07:45 +0900

    Refactor: button을 link로 변경

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 import { useState } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useSearchParams, Link } from 'react-router-dom';
 import Title from '../styles/Title';
 import { Button } from '../styles/Buttons';
 import CommunityList from '../components/CommunityList';
 import Pagination from '../components/Pagination';
+import { URL_WRITEPOST } from '../routesURL';
 
 const CommunityPage = styled.div`
 	display: flex;
@@ -22,17 +23,34 @@ const CommunityPage = styled.div`
 		margin-bottom: 20px;
 		align-items: end;
 	}
+	.postWrite {
+		height: 30px;
+		width: 100px;
+		padding: 3px;
+		background-color: var(--main-color);
+		color: var(--text-white-color);
+		box-shadow: 0px 2px 0px rgba(0, 0, 0, 0.25),
+			inset 0px 2px 0px rgba(255, 255, 255, 0.25);
+		border-radius: 5px;
+		font-size: var(--base);
+		font-weight: 700;
+		align-items: center;
+		display: flex;
+		justify-content: center;
+	}
+	.postWrite:hover {
+		background-color: var(--sub3-color);
+		color: var(--text-black-color);
+		box-shadow: 0px 2px 0px rgba(255, 255, 255, 0.25),
+			inset 0px 2px 0px rgba(0, 0, 0, 0.25);
+	}
 	@media (max-width: 768px) {
 		padding-top: 30px;
-	}
-`;
-const WriteBt = styled(Button)`
-	height: 30px;
-	font-size: var(--base);
-	display: flex;
-	justify-content: center;
-	@media (max-width: 786px) {
-		width: 80px;
+		.postWrite {
+			width: 80px;
+			height: 30px;
+			font-size: var(--base);
+		}
 	}
 `;
 
@@ -40,12 +58,13 @@ function Community() {
 	const [searchParams, setSearchParams] = useSearchParams();
 	const [currentPage, setCurrentPage] = useState(0);
 	const [totalPage, setTotalPage] = useState(19);
-	const navigate = useNavigate();
 	return (
 		<CommunityPage>
 			<div className="flex">
 				<Title>게시판</Title>
-				<WriteBt onClick={() => navigate('/post/write')}>글 작성</WriteBt>
+				<Link to={URL_WRITEPOST} className="postWrite">
+					글 작성
+				</Link>
 			</div>
 			<CommunityList totalPage={totalPage} />
 			{totalPage >= 0 ? (
