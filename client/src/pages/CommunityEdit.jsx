37eba1dbcf0721632077db8d254c4904dbf3aae4
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-05-11 01:08:19 +0900

    Test: 수정페이지 props로 받기

--- client/src/pages/CommunityEdit.jsx
+++ client/src/pages/CommunityEdit.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import CommunityEditor from '../components/CommunityEditor';
 import { Button, WarningButton } from '../styles/Buttons';
 import { postCommunity } from '../api/CommunityAPI';
@@ -34,30 +34,33 @@ const EditPage = styled.div`
 
 function CommunityEdit() {
 	const navigate = useNavigate();
-	const [titleBind] = useInput();
-	const [contentBind] = useInput();
+	const location = useLocation();
+	const [titleBind] = useInput(location.state ? location.state.p_title : '');
+	const [contentBind] = useInput(
+		location.state ? location.state.p_content : '',
+	);
+	// 수정 시 기존의 제목, 내용, postid props로 받기
 
 	// json서버 테스트용 실제서버는 url이랑 data 변경해야함
-	function writePost(method) {
-		if (method === 'post') {
-			postCommunity('/write', {
-				p_title: titleBind.value,
-				P_content: contentBind.value,
-				m_id: '작성자아이디',
-			});
-		}
-		if (method === 'patch') {
+	function writePost() {
+		if (location.state) {
 			postCommunity(
 				'/write/1',
 				{
 					p_title: titleBind.value,
 					P_content: contentBind.value,
 				},
-				method,
+				'patch',
 			);
+			navigate(`/post/read/${location.state.id}`);
+		} else {
+			postCommunity('/write', {
+				p_title: titleBind.value,
+				P_content: contentBind.value,
+				m_id: '작성자아이디',
+			});
+			// 응답에서 postid 받으면 navigate연결하기
 		}
-		// 작성, 수정한 게시글로 이동
-		// navigate('/post/read/{postid})
 	}
 	return (
 		<EditPage>
@@ -66,7 +69,7 @@ function CommunityEdit() {
 				<Button className="bt" onClick={() => navigate(-1)}>
 					작성 취소
 				</Button>
-				<WarningButton className="bt" onClick={() => writePost('patch')}>
+				<WarningButton className="bt" onClick={() => writePost()}>
 					작성 완료
 				</WarningButton>
 			</div>
