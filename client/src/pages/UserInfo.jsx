bd8518e894ccd58a40437a89797084dfdbc48145
전형호 <wjsgudgh12@naver.com>
2023-05-19 08:27:07 +0900

    remove: 필요없는 부분 삭제

--- client/src/pages/UserInfo.jsx
+++ client/src/pages/UserInfo.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
-import EditUserInfo from './EditUserInfo';
 
 function UserInfo() {
 	const [postList, setPostList] = useState([]);
