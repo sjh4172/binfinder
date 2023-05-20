5883ac14c98ece174216eabf0e5dc4e6e0b99322
Seoyeon Back <97516208+yeri134@users.noreply.github.com>
2023-05-20 17:41:38 +0900

    Update WebConfig.java

--- server/src/main/java/com/codestates/config/WebConfig.java
+++ server/src/main/java/com/codestates/config/WebConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://3.26.117.251:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(3600);
