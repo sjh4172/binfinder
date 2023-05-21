84ad99d44e538d0f2e2909f4a82db070d33d0610
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-05-21 11:53:45 +0900

    Refactor: alert을 모달로 변경

--- client/src/pages/CommunityEdit.jsx
+++ client/src/pages/CommunityEdit.jsx
@@ -1,21 +1,27 @@
 import styled from 'styled-components';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import CommunityEditor from '../components/CommunityEditor';
 import { Button, WarningButton } from '../styles/Buttons';
 import { postCommunity } from '../api/communityAPI';
 import useInput from '../hooks/useInput';
 import { URL_POST } from '../routesURL';
+import Modal from '../components/Modal';
 
 function CommunityEdit() {
 	const navigate = useNavigate();
 	const location = useLocation();
 	const [titleBind] = useInput(location.state && location.state.b_title);
 	const [contentBind] = useInput(location.state && location.state.b_content);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [message, setMessage] = useState('');
 	const writePost = () => {
 		if (!titleBind.value) {
-			// alert('제목을 작성해주세요.');
+			setMessage('제목을 작성해주세요.');
+			setIsModalOpen(true);
 		} else if (!contentBind.value) {
-			// alert('내용을 작성해주세요.');
+			setMessage('내용을 작성해주세요.');
+			setIsModalOpen(true);
 		} else if (location.state) {
 			postCommunity(
 				`/boards/${location.state.id}`,
@@ -34,6 +40,10 @@ function CommunityEdit() {
 			// TODO: 응답에서 postid 받으면 navigate로 작성글, 아니라면 전체목록 연결하기
 		}
 	};
+	const handleConfirm = () => {
+		setIsModalOpen(false);
+	};
+
 	return (
 		<EditPageContainer>
 			<CommunityEditor contentBind={contentBind} titleBind={titleBind} />
@@ -45,6 +55,9 @@ function CommunityEdit() {
 					작성 완료
 				</WarningButton>
 			</div>
+			{isModalOpen && (
+				<Modal message={message} handleConfirm={handleConfirm} cancel={false} />
+			)}
 		</EditPageContainer>
 	);
 }
