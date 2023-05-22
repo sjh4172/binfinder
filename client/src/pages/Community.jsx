094842d0ad17569ffaeedd114071038e8a9ec808
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-05-22 11:27:35 +0900

    Test: 총페이지 받아오기

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -20,18 +20,33 @@ function Community() {
 	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
 	useEffect(() => {
-		if (location.search) {
-			getPostList(location.search).then((res) => {
-				setData(res.data);
-				setTotalPage(res.headers.get('X-Total-Pages'));
-			});
-		}
+		const fetchData = async () => {
+			if (location.search) {
+				try {
+					const res = await getPostList(location.search);
+					setData(res.data);
+					setTotalPage(res.headers.get('X-Total-Pages'));
+				} catch (error) {
+					// Handle error
+				}
+			}
+		};
+		fetchData();
 	}, [searchParams]);
 
+	// useEffect(() => {
+	// 	if (location.search) {
+	// 		getPostList(location.search).then((res) => {
+	// 			setData(res.data);
+	// 			setTotalPage(res.headers.get('X-Total-Pages'));
+	// 		});
+	// 	}
+	// });
 	const handleConfirm = () => {
 		setIsModalOpen(false);
 	};
 
+	console.log(totalPage);
 	return (
 		<CommunityPageContainer>
 			<div className="flex">
